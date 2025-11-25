1aaf1897152d2de7b2091f7cef33e501b3e8f640
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-26 00:57:18 +0530

    Update link.py

--- SONALI_MUSIC/plugins/tools/link.py
+++ SONALI_MUSIC/plugins/tools/link.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, filters
+from SONALI_MUSIC.core.client import app
+from pyrogram import filters
 from pyrogram.errors import ChatAdminRequired
 
 @app.on_message(filters.command(["givelink", "getlink", "link"]))
@@ -8,16 +9,16 @@ async def give_link(bot, message):
         return await message.reply("❌ Usage: /link <chat_id>")
 
     try:
-        chat_id = int(message.command[1])  # << IMPORTANT FIX
+        chat_id = int(message.command[1])
 
         link = await bot.export_chat_invite_link(chat_id)
         await message.reply(f"✅ Chat Invite Link:\n{link}")
 
     except ValueError:
-        await message.reply("❌ Invalid chat_id format!")
+        await message.reply("❌ Invalid chat_id format! Chat ID must be a number.")
 
     except ChatAdminRequired:
-        await message.reply("❌ Bot admin nahi hai ya invite permission nahi hai!")
+        await message.reply("❌ Bot admin nahi hai ya invite permission nahi hai is chat me!")
 
     except Exception as e:
         await message.reply(f"⚠️ Error: {e}")
