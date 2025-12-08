cae36d7152a28e18ea49b833b1aa8af0e1df7af1
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-09 00:56:14 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -64,7 +64,7 @@ def stream_markup_timer(_, chat_id, played, dur):
          InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/lll_TOXICC_PAPA_lll"),
+         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/lll_TOXICC_PAPA_lll"),
          InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨•", url="https://t.me/ytxmusic_bot?start=_tgr_JCUTUrY0YjQ1"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -75,7 +75,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/lll_TOXICC_PAPA_lll"),
+         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/lll_TOXICC_PAPA_lll"),
          InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/ytxmusic_bot?start=_tgr_JCUTUrY0YjQ1"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
