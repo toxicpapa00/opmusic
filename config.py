8f6ee77294adc25fd77430563c24c45d2bcf3998
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-10 22:43:53 +0530

    Change API_KEY in config.py
    Updated API_KEY to a new value.

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots254c6b') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+API_KEY = getenv("API_KEY", 'xbit_8HHTaoWfgIetEr55u4752ASJyaCVLUpN') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-YukkiMusic-08-30")
