5ebd0b41820be9ad0b60ef2d5e9af1a456ddd180
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-10 23:05:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/toxicpapa00/opmusic"
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_URL = getenv("API_URL", 'https://music.xbitcode.com') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.music.xbitcode.com')
 API_KEY = getenv("API_KEY", 'xbit_8HHTaoWfgIetEr55u4752ASJyaCVLUpN') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 
